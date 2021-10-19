bd8ac66dabbefb97cfe3be7c25376d5fa33cc4f2
Ahren Bader-Jarvis <djahren@djahren.com>
2021-10-19 00:20:24 -0700

    remove configurator dep on prompt-sync

--- configurator.js
+++ configurator.js
@@ -1,20 +1,30 @@
 const fs = require('fs');
-try{
-    var prompt = require('prompt-sync')();
-} catch{
-    console.log("Please make sure Node is installed and run 'npm install' first.");
-    process.exit()
-}
+const rl = require('readline')
 var config = require('./config.json');
 var colors = {
     "red": "\x1b[31m%s\x1b[0m",
     "yellow": "\x1b[33m%s\x1b[0m",
     "cyan": "\x1b[36m%s\x1b[0m"
 }
-function confirmYesNo(question){
+
+async function prompt(question){ //modifed from https://stackoverflow.com/a/46700053
+    const readline = rl.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    var promptInput = await new Promise((resolve, reject) => {
+        readline.question(question, (answer) => {
+            resolve(answer.trim());
+        });
+    });
+    readline.close();
+    return promptInput;
+}
+
+async function confirmYesNo(question){
     var selection = ""; 
     while(selection != "y" && selection != "n"){
-        selection = prompt(question + ' [Y/N]: ').trim().toLowerCase()
+        selection = await prompt(question + ' [Y/N]: '); selection = selection.toLowerCase()
         switch (selection) {
             case "y":
                 return true;
@@ -27,6 +37,7 @@ function confirmYesNo(question){
     }
 }
 
+async function configurator(){
 //1. follow instructions to add bot to server
 console.log(colors.cyan,"Welcome to the DiscordDrinkingJenga configurator!")
 console.log("We'll help you get your bot setup and ready to play!")
@@ -49,10 +60,10 @@ if(fs.existsSync(auth_fn)){
             token = auth.token;
             if(auth.token.match(tokenRegex)){
                 console.log("Your token is already set in auth.json and appears to be valid.")
-                needToken = confirmYesNo("Would you like to update it?")
+                needToken = await confirmYesNo("Would you like to update it?")
             } else if(!auth.token.match(tokenRegex)){
                 console.log(colors.red, "Your token is already set in auth.json and but does NOT appear to be valid.")
-                needToken = confirmYesNo("Would you like to update it?")
+                needToken = await confirmYesNo("Would you like to update it?")
             }
         } 
     } catch {
@@ -61,14 +72,14 @@ if(fs.existsSync(auth_fn)){
 }
 
 while(needToken){
-    token = prompt('Please paste your bot auth token: ').trim()
+    token = await prompt('Please paste your bot auth token: ')
     if(token){
         if(token.match(tokenRegex)){
             needToken = false
             break;
         } else {
             console.log(colors.yellow,"WARNING: This token may not be valid.")
-            needToken = confirmYesNo('Would you like to re-enter it?')
+            needToken = await confirmYesNo('Would you like to re-enter it?')
         }
     } else {
         console.log(colors.red,"Invalid token.");
@@ -84,13 +95,13 @@ var selection = ""; var currentAdmins = config.permAdmins;
 while(selection != "3"){
     console.log(`\nThere are currently ${config.permAdmins.length} permanent admins specified. Would you like to: `)
     console.log("1. Add another admin.\n2. Wipe the admin list and start fresh.\n3. Continue to the next step.")
-    selection = prompt('Make a selection [1-3]: ').trim()
+    selection = await prompt('Make a selection [1-3]: ')
     console.log()
     switch (selection) {
         case "1":
             console.log("1. Add another admin. Get the user's Discord ID and paste it below.")
             console.log("If you don't know how to get a user ID, visit: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-")
-            var adminID = prompt('Paste the ID of an admin: ').trim()
+            var adminID = await prompt('Paste the ID of an admin: ')
             if(adminID.match(/\d{18}/)){
                 config.permAdmins.push(adminID)
             }
@@ -119,7 +130,7 @@ console.log(colors.cyan,"\nStep 4 (Optional): Set dedicated channel ID.")
 var currentChannelID = config.channelID;
 var newChannelID; var channelIDSet = false;
 while(!channelIDSet){
-    newChannelID = prompt("Paste the dedicated channel ID (press Enter to skip): ").trim();
+    newChannelID = await prompt("Paste the dedicated channel ID (press Enter to skip): ");
     if(newChannelID){
         if(newChannelID.match(/\d{18}/)){
             config.channelID = newChannelID;
@@ -142,14 +153,14 @@ if(config.commandPrefix == "!"){
 } else {
     console.log(`The current commandPrefix is: ${config.commandPrefix} (the default is: !)`)
 }
-var newPrefix = prompt("Set a new commandPrefix? (press Enter to skip): ").trim()
+var newPrefix = await prompt("Set a new commandPrefix? (press Enter to skip): ")
 if(newPrefix){
     config.commandPrefix = newPrefix;
 }
 
 //6. Save changes? 
 console.log(colors.cyan,"\nStep 6: Save your changes!")
-if(confirmYesNo('Would you like to save your changes to config.json?')){
+if(await confirmYesNo('Would you like to save your changes to config.json?')){
     fs.writeFileSync(auth_fn,JSON.stringify(auth));
     fs.writeFileSync("./config.json",JSON.stringify(config, null, 4));
 }
@@ -157,3 +168,6 @@ if(confirmYesNo('Would you like to save your changes to config.json?')){
 //7. instruct to run npm start
 console.log(colors.cyan,"\nStep 7: Let's Drink!")
 console.log("You're all done with setup. Type 'npm start' to run the bot.\nIf all went well, you should see your bot come online, if you specified a dedicated channel the bot will send a message there letting you know it's online.\n")
+}
+
+configurator();
